9ef98da9bfc70c8a05ba00664bb8f46b45e332f2
tomcw <tomcw@users.noreply.github.com>
2020-09-06 11:27:21 +0100

    CybernoidTest: added comments for where noisy problem occurs Cybernoid: Updated build.bat Updated MB-Macros.a to not attenuate if amp.mode=envelope . Nb. neither cybernoid1/2 use envelopes

--- Cybernoid/build.bat
+++ Cybernoid/build.bat
@@ -1,4 +1,2 @@
-D:\Apple][\acme085\acme.exe cybernoid.a
-D:\Apple][\InsertBIN2AWS\debug\InsertBIN2AWS.exe cybernoid 6000 cybernoid.aws
-java -jar "D:\Apple][\Apple Commander\ac\AppleCommander-1.3.3.9-ac.jar" -d cybernoid ..\ReleaseDSK\Cybernoid.dsk
-cat cybernoid | java -jar "D:\Apple][\Apple Commander\ac\AppleCommander-1.3.3.9-ac.jar" -p Cybernoid bin 24576 ..\ReleaseDSK\Cybernoid.dsk
+acme.exe cybernoid.a
+copy cybernoid.labels %APPLEWIN_DBG%\A2_USER1.sym
