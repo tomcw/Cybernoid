f3e394202f10f6b3940e6c8f711bc01447d95078
tomcw <tomcw@users.noreply.github.com>
2024-09-30 21:02:01 +0100

    Build both Cybernoid (for MB) and Cybernoid.PH (for Phasor)

--- Cybernoid/build.bat
+++ Cybernoid/build.bat
@@ -1,2 +1,6 @@
-acme.exe cybernoid.a
+acme.exe -DUSE_PHASOR=1 cybernoid.a
+@REM copy cybernoid.labels %APPLEWIN_DBG%\A2_USER1.sym
+move /y Cybernoid CYBERNOID.PH
+
+acme.exe -DUSE_PHASOR=0 cybernoid.a
 copy cybernoid.labels %APPLEWIN_DBG%\A2_USER1.sym
